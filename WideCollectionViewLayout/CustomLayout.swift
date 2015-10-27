1e69e365a94cfc748f10530a67e778192b292d71
Mike Abdullah <me@mikeabdullah.net>
2015-10-27 10:04:37 +0000

    44pt items, section insets to achieve 7 across layout

--- WideCollectionViewLayout/CustomLayout.swift
+++ WideCollectionViewLayout/CustomLayout.swift
@@ -20,25 +20,24 @@ class CustomLayout: UICollectionViewFlowLayout {
         super.prepareLayout()
         
         if let collectionView = self.collectionView {
+            
+            let width = collectionView.bounds.size.width
+            
             var newItemSize = itemSize
 
             // Always use an item count of at least 1 and convert it to a float to use in size calculations
             let itemsPerRow = CGFloat(max(numberOfItemsPerRow, 1))
             
-            // Calculate the sum of the spacing between cells
-            let totalSpacing = minimumInteritemSpacing * (itemsPerRow - 1.0)
-            
             // Calculate how wide items should be
-			newItemSize.width = (collectionView.bounds.size.width - sectionInset.left - sectionInset.right - totalSpacing) / itemsPerRow
-            
-            // Use the aspect ratio of the current item size to determine how tall the items should be
-            if itemSize.height > 0 {
-                let itemAspectRatio = itemSize.width / itemSize.height
-                newItemSize.height = newItemSize.width / itemAspectRatio
-            }
+            newItemSize.width = 44; newItemSize.height = 44;
             
             // Set the new item size
             itemSize = newItemSize
+            
+            var newInsets = sectionInset
+            newInsets.left = (width - (itemsPerRow * newItemSize.width) - ((itemsPerRow-1) * minimumInteritemSpacing)) / 2
+            newInsets.right = newInsets.left;
+            sectionInset = newInsets
         }
     }
 }
