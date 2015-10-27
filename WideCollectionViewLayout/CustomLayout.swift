cdf74ce4e240826e8bcd944990d1d56209ca0853
Mike Abdullah <me@mikeabdullah.net>
2015-10-27 10:21:54 +0000

    comments

--- WideCollectionViewLayout/CustomLayout.swift
+++ WideCollectionViewLayout/CustomLayout.swift
@@ -28,12 +28,11 @@ class CustomLayout: UICollectionViewFlowLayout {
             // Always use an item count of at least 1 and convert it to a float to use in size calculations
             let itemsPerRow = CGFloat(max(numberOfItemsPerRow, 1))
             
-            // Calculate how wide items should be
+            // Constant size items
             newItemSize.width = 44; newItemSize.height = 44;
-            
-            // Set the new item size
             itemSize = newItemSize
             
+            // Bring in section insets to cluster the cells as close together as possible
             var newInsets = sectionInset
             newInsets.left = (width - (itemsPerRow * newItemSize.width) - ((itemsPerRow-1) * minimumInteritemSpacing)) / 2
             newInsets.right = newInsets.left;
